5a039c8f2505f2cb437b574d228a2ff6446e8cfc
bmwtsn098 <bmwtsn99@gmail.com>
2022-10-29 23:51:54 -0700

    add GitHub action to launch Mayhem

---
+++ mayhem/fuzz.go
@@ -0,0 +1,17 @@
+package fuzz
+
+import "github.com/apache/calcite-avatica-go/v5"
+
+
+func mayhemit(bytes []byte) int {
+
+    content := string(bytes)
+    var test avatica.Driver
+    test.Open(content)
+    return 0
+}
+
+func Fuzz(data []byte) int {
+    _ = mayhemit(data)
+    return 0
+}
